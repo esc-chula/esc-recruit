f7137d90b1ad937573b317d69aa64f0a00ec99c5
ebonian <tanadon.santisan@gmail.com>
2024-05-15 03:08:09 +0700

    feat: 67 home page

--- app/[year]/page.tsx
+++ app/[year]/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from "next/navigation";
 import Years from "@/data/years.json";
+import Image from "next/image";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   return Years.map((year) => ({
@@ -14,5 +16,43 @@ export default function YearPage({ params }: { params: { year: string } }) {
     return notFound();
   }
 
-  return <div>{params.year}</div>;
+  return (
+    <div className="flex flex-col items-center h-full w-full justify-center space-y-16">
+      <div className="flex flex-col items-center space-y-6">
+        <Image
+          src="/logo.svg"
+          width={96}
+          height={96}
+          alt={"esc logo"}
+          className="select-none"
+        />
+        <div className="text-center space-y-4">
+          <h1 className="font-bold leading-8">
+            <span className="text-3xl">We’re Recruiting!</span>
+            <br />
+            <span className="text-2xl">รับสมัครเพื่อนร่วมทีม กวศ.67</span>
+          </h1>
+          <p className="text-sm">
+            ตั้งแต่วันนี้ - 20 พฤษภาคม 2567
+            <br />
+            โดยจะมีการสัมภาษณ์ตั้งแต่วันที่ 27-31 พฤษภาคม 2567
+          </p>
+        </div>
+      </div>
+      <div className="grid text-center space-y-4">
+        <Link
+          href="/"
+          className="px-4 py-2 bg-identity-cloud text-identity-esc rounded-xl font-semibold text-xl"
+        >
+          ฟอร์มรับสมัคร
+        </Link>
+        <Link
+          href={`/${year.id}/test/1`}
+          className="px-4 py-2 border-2 border-identity-cloud rounded-xl font-medium text-sm"
+        >
+          เราเหมาะกับฝ่ายไหนใน กวศ. กันนะ?
+        </Link>
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import Years from "@/data/years.json";
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
@@ -15,5 +16,15 @@ export default function Home() {
     }
   }, [activeYears, router]);
 
-  return null;
+  return (
+    <div className="flex flex-col items-center h-full w-full justify-center">
+      <Image
+        src="/logo.svg"
+        width={64}
+        height={64}
+        alt={"esc logo"}
+        className="select-none animate-pulse duration-0"
+      />
+    </div>
+  );
 }
