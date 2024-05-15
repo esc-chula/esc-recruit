58e689e439399417f70b177765476f9c64dc05c4
ebonian <tanadon.santisan@gmail.com>
2024-05-15 21:42:43 +0700

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="th">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta
           content="width=device-width, initial-scale=1, maximum-scale=1"
           name="viewport"
