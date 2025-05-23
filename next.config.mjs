e9e727a63b9b9b34c61a8d914d103986f132bf15
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2025-05-24 01:07:40 +0700

    feat: add Dockerfile with next config setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    compress: true,
+    output: 'standalone',
+    compiler: {
+        removeConsole: true,
+      },
+};
 
 export default nextConfig;
